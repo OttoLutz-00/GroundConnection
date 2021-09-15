121483b21628cfbb57a61bcecb3320c9ab208aa4
adegboyefoluso <foluso.o.adegboye@gmail.com>
2021-09-15 17:45:51 -0400

    Added Get By Id

---
+++ GroundConnection.Models/JobApplication/JobApplicationListItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroundConnection.Models.JobApplication
+{
+   public  class JobApplicationListItem
+    {
+        public string UserName { get; set; }
+        public string Skills { get; set; }
+        public DateTimeOffset? AcceptedDate { get; set; }
+        public int Id { get; set; }
+
+
+    }
+}
